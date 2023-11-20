18a72f7a423d3c45f168b3e65a0c1099ea91d85b
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2023-11-20 10:03:11 +0700

    ✨ feat(online-pay.module.ts): import CardsModule and TransactionsModule to enable card transactions in online payments

--- src/modules/pay/online-pay.module.ts
+++ src/modules/pay/online-pay.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OnlinePayController } from './online-pay.controller';
 import { OmiseService } from './omise.service';
+import { CardsModule } from '../cards/cards.module';
+import { TransactionsModule } from '../transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [CardsModule, TransactionsModule],
   controllers: [OnlinePayController],
   providers: [OmiseService],
 })
